a613c908ec7635d69853feb60617ed1b7868a0f6
shazonglin <shazonglin46@163.com>
2023-05-29 16:56:17 +0800

    main page docker layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="docker">
-    <div class="docker-item">首页</div>
-    <div class="docker-item">购物车</div>
-    <div class="docker-item">订单</div>
-    <div class="docker-item">我的</div>
+    <div class="docker_item docker_item--active">
+      <div class="iconfont">&#xe617;</div>
+      <div class="docker_title">首页</div>
+    </div>
+    <div class="docker_item">
+      <div class="iconfont">&#xe6af;</div>
+      <div class="docker_title">购物车</div>
+    </div>
+    <div class="docker_item">
+      <div class="iconfont">&#xe608;</div>
+      <div class="docker_title">订单</div>
+    </div>
+    <div class="docker_item">
+      <div class="iconfont">&#xe78b;</div>
+      <div class="docker_title">我的</div>
+    </div>
   </div>
 </template>
 
@@ -17,11 +29,22 @@
   bottom: 0;
   width: 100%;
   height: 0.49rem;
-  background-color: rgb(203, 236, 203);
-}
-
-.docker-item {
-  flex: 1;
-  text-align: center;
+  border-top: 1px solid #f1f1f1;
+  &_item {
+    flex: 1;
+    text-align: center;
+    .iconfont {
+      margin: 0.07rem 0 0.02rem 0;
+      font-size: 0.18rem;
+    }
+    &--active {
+      color: #1fa4fc;
+    }
+  }
+  &_title {
+    font-size: 20px;
+    transform: scale(0.5, 0.5);
+    transform-origin: center top;
+  }
 }
 </style>
